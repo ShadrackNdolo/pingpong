27265692a6579666784742317752a2de94979e04
Ndolo S <shadrack.winters@outlook.com>
2017-08-18 16:17:55 +0300

    Final touches

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+var pingPong = function (number) {
+  var numberArray = [];
+  for (var i = 1; i <= number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      numberArray.push("pingpong");
+    }
+    else if (i % 5 === 0) {
+      numberArray.push("pong");
+    }
+    else if (i % 3 === 0) {
+      numberArray.push("ping");
+    }
+    else {
+      numberArray.push(i);
+    }
+  }
+  return numberArray;
+};
+
+$(document).ready(function() {
+  $("form#userNumber").submit(function(event) {
+
+   var number = parseInt($("input#number").val());
+    if (number <= 0){
+      alert("Please enter a number greater than or equal to 1!");
+    }
+    var returnedNumberArray = pingPong(number);
+
+   $("ul").empty();
+
+   for (var i = 0; i < returnedNumberArray.length; i++) {
+      var listItem = document.createElement("li");
+      listItem.innerHTML = returnedNumberArray[i];
+      var numberList = document.getElementById("ping-pong");
+      numberList.appendChild(listItem);
+    }
+
+   $("#result").show();
+
+   event.preventDefault();
+  });
+});
